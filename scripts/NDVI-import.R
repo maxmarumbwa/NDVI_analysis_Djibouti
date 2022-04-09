fc54059608cebd60c7f2a42987d9fa388e09e8e7
faraigit <faraimax@gmail.com>
2022-04-09 23:54:41 +0200

    raster stats and saving raster stack manatining layer names

--- scripts/NDVI-import.R
+++ scripts/NDVI-import.R
@@ -9,8 +9,8 @@ library(dplyr)
 
 
 #Load shp + use this to seclect polygon to plot
-inshp<- readOGR(here("data","shp","Djibouti-adm2.shp"))
-shp <- inshp[inshp$ADM2_NAME %in% c( "Randa","Tadjourah"), ]
+shp<- readOGR(here("data","shp","Djibouti-adm2.shp"))
+inshp <- inshp[inshp$ADM2_NAME %in% c( "Randa","Tadjourah"), ]
 plot(shp)
 
 # list ndvi files
@@ -56,7 +56,84 @@ df_ndvi_2$year <- as.integer(df_ndvi_2$year)
 # temp_data %>% #   mutate( season = case_when(month %in%  9:11 ~ "Fall", month %in%  c(12, 1, 2)  ~ "Winter",
 #       month %in%  3:5  ~ "Spring",  TRUE ~ "Summer"))
 
-###### Create files for each year ###### 
+###### Create files List and raster stack for each year ###### 
+
+######------------- Create files for each year------------ ###### 
+#feb_jun_2002 <- df_ndvi_2 %>% filter(year == 2002 & season == "main season")
+feb_jun_2003 <- df_ndvi_2 %>% filter(year == 2003 & season == "main season")
+feb_jun_2004 <- df_ndvi_2 %>% filter(year == 2004 & season == "main season")
+feb_jun_2005 <- df_ndvi_2 %>% filter(year == 2005 & season == "main season")
+feb_jun_2006 <- df_ndvi_2 %>% filter(year == 2006 & season == "main season")
+feb_jun_2007 <- df_ndvi_2 %>% filter(year == 2007 & season == "main season")
+feb_jun_2008 <- df_ndvi_2 %>% filter(year == 2008 & season == "main season")
+feb_jun_2009 <- df_ndvi_2 %>% filter(year == 2009 & season == "main season")
+feb_jun_2010 <- df_ndvi_2 %>% filter(year == 2010 & season == "main season")
+feb_jun_2011 <- df_ndvi_2 %>% filter(year == 2011 & season == "main season")
+feb_jun_2012 <- df_ndvi_2 %>% filter(year == 2012 & season == "main season")
+feb_jun_2013 <- df_ndvi_2 %>% filter(year == 2013 & season == "main season")
+feb_jun_2014 <- df_ndvi_2 %>% filter(year == 2014 & season == "main season")
+feb_jun_2015 <- df_ndvi_2 %>% filter(year == 2015 & season == "main season")
+feb_jun_2016 <- df_ndvi_2 %>% filter(year == 2016 & season == "main season")
+feb_jun_2017 <- df_ndvi_2 %>% filter(year == 2017 & season == "main season")
+feb_jun_2018 <- df_ndvi_2 %>% filter(year == 2018 & season == "main season")
+feb_jun_2019 <- df_ndvi_2 %>% filter(year == 2019 & season == "main season")
+feb_jun_2020 <- df_ndvi_2 %>% filter(year == 2020 & season == "main season")
+feb_jun_2021 <- df_ndvi_2 %>% filter(year == 2021 & season == "main season")
+
+# ---------- Create raster stack-------------------
+wd <- here("data","ndvi","/")
+
+#r2002<- stack(paste0(wd, feb_jun_2002$img_date)) %>% raster::crop(raster::extent(shp)) %>% raster::mask(shp) %>% mean(na.rm = TRUE)
+r2003<- stack(paste0(wd, feb_jun_2003$img_date)) %>% raster::crop(raster::extent(shp)) %>% raster::mask(shp) %>% mean(na.rm = TRUE)
+r2004<- stack(paste0(wd, feb_jun_2004$img_date)) %>% raster::crop(raster::extent(shp)) %>% raster::mask(shp) %>% mean(na.rm = TRUE)
+r2005<- stack(paste0(wd, feb_jun_2005$img_date)) %>% raster::crop(raster::extent(shp)) %>% raster::mask(shp) %>% mean(na.rm = TRUE)
+r2006<- stack(paste0(wd, feb_jun_2006$img_date)) %>% raster::crop(raster::extent(shp)) %>% raster::mask(shp) %>% mean(na.rm = TRUE)
+r2007<- stack(paste0(wd, feb_jun_2007$img_date)) %>% raster::crop(raster::extent(shp)) %>% raster::mask(shp) %>% mean(na.rm = TRUE)
+r2008<- stack(paste0(wd, feb_jun_2008$img_date)) %>% raster::crop(raster::extent(shp)) %>% raster::mask(shp) %>% mean(na.rm = TRUE)
+r2009<- stack(paste0(wd, feb_jun_2009$img_date)) %>% raster::crop(raster::extent(shp)) %>% raster::mask(shp) %>% mean(na.rm = TRUE)
+r2010<- stack(paste0(wd, feb_jun_2010$img_date)) %>% raster::crop(raster::extent(shp)) %>% raster::mask(shp) %>% mean(na.rm = TRUE)
+r2011<- stack(paste0(wd, feb_jun_2011$img_date)) %>% raster::crop(raster::extent(shp)) %>% raster::mask(shp) %>% mean(na.rm = TRUE)
+r2012<- stack(paste0(wd, feb_jun_2012$img_date)) %>% raster::crop(raster::extent(shp)) %>% raster::mask(shp) %>% mean(na.rm = TRUE)
+r2013<- stack(paste0(wd, feb_jun_2013$img_date)) %>% raster::crop(raster::extent(shp)) %>% raster::mask(shp) %>% mean(na.rm = TRUE)
+r2014<- stack(paste0(wd, feb_jun_2014$img_date)) %>% raster::crop(raster::extent(shp)) %>% raster::mask(shp) %>% mean(na.rm = TRUE)
+r2015<- stack(paste0(wd, feb_jun_2015$img_date)) %>% raster::crop(raster::extent(shp)) %>% raster::mask(shp) %>% mean(na.rm = TRUE)
+r2016<- stack(paste0(wd, feb_jun_2016$img_date)) %>% raster::crop(raster::extent(shp)) %>% raster::mask(shp) %>% mean(na.rm = TRUE)
+r2017<- stack(paste0(wd, feb_jun_2017$img_date)) %>% raster::crop(raster::extent(shp)) %>% raster::mask(shp) %>% mean(na.rm = TRUE)
+r2018<- stack(paste0(wd, feb_jun_2018$img_date)) %>% raster::crop(raster::extent(shp)) %>% raster::mask(shp) %>% mean(na.rm = TRUE)
+r2019<- stack(paste0(wd, feb_jun_2019$img_date)) %>% raster::crop(raster::extent(shp)) %>% raster::mask(shp) %>% mean(na.rm = TRUE)
+r2020<- stack(paste0(wd, feb_jun_2020$img_date)) %>% raster::crop(raster::extent(shp)) %>% raster::mask(shp) %>% mean(na.rm = TRUE)
+r2021<- stack(paste0(wd, feb_jun_2021$img_date)) %>% raster::crop(raster::extent(shp)) %>% raster::mask(shp) %>% mean(na.rm = TRUE)
+
+# TEST
+
+# Seasonal NDVI raster stack ,  Set pixels outside the mask to null
+season_ndvi <- stack(r2003,r2004,r2005,r2006, r2007,r2008,r2009,r2010,r2011,r2012,r2013,r2014,r2015,r2016,r2017,r2018,r2019,r2020,r2021)
+#season_ndvi <- stack(r2002,r2003,r2004,r2005,r2006, r2007,r2008,r2009,r2010,r2011,r2012,r2013,r2014,r2015,r2016,r2017,r2018,r2019,r2020,r2021)
+season_ndvi[season_ndvi > 0.1] <- NA
+#new_names <- paste0("ndvi_", 2002:2021)
+names(season_ndvi)
+new_names <- paste0("ndvi_", 2003:2021)
+names(season_ndvi) <-  new_names
+#levelplot(season_ndvi)
+summary(season_ndvi)
+
+# Calculate average
+season_avg <- max(season_ndvi,na.rm = TRUE)
+season_avg[season_avg > 0.1]<- NA # Set pixels outside the mask to null
+
+#-------------------------- print image stats
+summary(season_ndvi)
+summary(season_avg)
+
+# Save the seasonal avg and the longterm avg ndvi
+raster::writeRaster(season_avg, filename= here("out", "tif","NDVI_max_Feb_Jun_2003_2021.tif"), format="GTiff", bylayer=TRUE, overwrite=TRUE)
+raster::writeRaster(season_ndvi, filename=here("out", "tif","NDVI_Feb_June_2003_2021_brick.tif"), format="GTiff", bylayer=TRUE, overwrite=TRUE)
+
+
+
+
+
+
 ndvi_major_minor_season <-split(df_ndvi_2,list(df_ndvi_2$year,df_ndvi_2$season, df_ndvi_2$img_path))
 head(ndvi_major_minor_season)
 ndvi_full_season <-split(df_ndvi_2,list(df_ndvi_2$year,df_ndvi_2$season_full, df_ndvi_2$img_path))
@@ -83,6 +160,17 @@ groupings      # show unique groupings
 linelist_split %>% 
   writexl::write_xlsx(path = here("out", "File_path_df.xlsx"))
 
+########----------- Filter the list------------
+z=linelist_split %>% lapply(., "[", , 'img_path')
+names(z)[lapply(z, names) == "2006-Missing"]
+
+
+
+
+l[lapply(l, length) > 0]
+
+
+
 #--------------Get the colum with the img path---------------------------
 z=linelist_split %>% lapply(., "[", , 'img_path')
 z
@@ -99,21 +187,43 @@ z
 #   1 C:/Users/Farai.marumbwa/OneDrive - World Food Programme/ARC_WORK/TASKS-TSD and RD/NDVI_analysis_Djibouti/data/n~
 
 #--------------create raster stack based on the new list---------------------------
-> stack(a[[1]][[1]])
+stack(z[[1]][[1]])
 # class      : RasterStack 
 # dimensions : 301, 301, 90601, 10  (nrow, ncol, ncell, nlayers)
 # resolution : 0.01, 0.01  (x, y)
 # extent     : 40.99, 44, 9.99, 13  (xmin, xmax, ymin, ymax)
 # crs        : +proj=longlat +datum=WGS84 +no_defs 
+#ap(rnorm, n = 10)
 # names      : DJ_MODAPE10_2002.07.21, DJ_MODAPE10_2002.08.01, DJ_MODAPE10_2002.08.11, DJ_MODAPE10_2002.08.21, DJ_MODAPE10_2002.09.01, DJ_MODAPE10_2002.09.11, DJ_MODAPE10_2002.09.21, DJ_MODAPE10_2002.10.01, DJ_MODAPE10_2002.10.11, DJ_MODAPE10_2002.10.21 
 
 # --------only save one raster
 b=list()
 i=1
 for (i in 1:20){
- b= stack(a[[i]][[i]])
+ b= stack(z[[i]][[i]])
  i=i+1
 }
+#-- this only saves the first raster stack
+bb=lapply(z, function(x) { x["ind"] <- NULL; x })%>%
+ 
+  
+bb%>% map(function(x) stack(x))
+
+
+addTen <- function(.x) {
+  return(stack(.x))
+}
+addTen(bb)
+
+library(tidyverse)
+map(.x = bb, 
+    .f = addTen)
+
+#The map() function below iterates addTen() across all entries of the vector, .x = c(1, 4, 7), and returns the output as a list
+
+library(tidyverse)
+map(.x = c(1, 4, 7), 
+    .f = addTen)
 
 
 #------------------ Select the first 27 items of the first col
